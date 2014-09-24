cb4ec2f88a8101f8a6d83bb0210ad6a2c2dbb2e0
Dave Musicant <dmusican@carleton.edu>
2014-09-24 11:02:47 -0500

    lets and other

---
+++ racket3.rkt
@@ -0,0 +1,46 @@
+#lang racket
+(define addOne
+  (lambda (x)
+    (+ x 1)))
+
+
+(foldl + 0 '(12 2 1 5))
+(foldl - 0 '(1 2 3))
+
+
+(define my-map
+  (lambda (f lst)
+    (if (null? lst)
+        '()
+        (cons (f (car lst))
+              (my-map f (cdr lst))))))
+(my-map addOne '(12 2 -3 0 9))
+
+(define my-foldl
+  (lambda (f init lst)
+    (if (null? lst)
+        init
+        (my-foldl f  [f (car lst) init] [cdr lst]))))
+
+(define my-foldr
+  (lambda (f init lst)
+    (if (null? lst)
+        init
+        (f (car lst) (my-foldr f init (cdr lst))))))
+
+
+(my-foldl cons '() '(1 2 3 4))
+(my-foldr cons '() '(1 2 3 4))
+
+;; let, let*, letrec, WTF???
+(define doit
+  (lambda (x)
+    (let ([a 3] [b 5])
+      (+ a b x))))
+
+
+(define doit2
+  (lambda (x)
+    (let ([a 3] [b (+ a 1)])
+      (+ a b x))))
+
